ee664657cf6e6d548def0b96f326b3504a927a5e
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2024-01-10 17:46:56 -0300

    fullstack

--- Program.cs
+++ Program.cs
@@ -21,6 +21,22 @@ builder.Services.AddSwaggerGen(c =>
   });
 });
 
+// CORS
+// 1) define a unique string
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// 2) define allowed domains, in this case "http://example.com" and "*" = all
+//    domains, for testing purposes only.
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+    builder =>
+    {
+      builder.WithOrigins(
+          "http://example.com", "*");
+    });
+});
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -28,16 +44,18 @@ app.UseSwaggerUI(c =>
   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 
+// use the capability
+app.UseCors(MyAllowSpecificOrigins);
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
-app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) =>
 {
   await db.Pizzas.AddAsync(pizza);
   await db.SaveChangesAsync();
-  return Results.Created($"/pizza/{pizza.Id}", pizza);
+  return Results.Created($"/pizzas/{pizza.Id}", pizza);
 });
-app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
-app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+app.MapGet("/pizzas/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizzas/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
 {
   var pizza = await db.Pizzas.FindAsync(id);
   if (pizza is null) return Results.NotFound();
@@ -46,7 +64,7 @@ app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
   await db.SaveChangesAsync();
   return Results.NoContent();
 });
-app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+app.MapDelete("/pizzas/{id}", async (PizzaDb db, int id) =>
 {
   var pizza = await db.Pizzas.FindAsync(id);
   if (pizza is null)
