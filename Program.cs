9c1b9bbb888941be3873d0799831abeaf9886217
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2024-01-10 15:45:32 -0300

    postgres database

--- Program.cs
+++ Program.cs
@@ -5,9 +5,12 @@ using PizzaStore.Models;
 // using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PizzaDbConnection") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+// builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddDbContext<PizzaDb>(options =>
+    options.UseNpgsql(connectionString));
 builder.Services.AddSwaggerGen(c =>
 {
   c.SwaggerDoc("v1", new OpenApiInfo
--- obj/Debug/net8.0/crud.AssemblyInfo.cs
+++ obj/Debug/net8.0/crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d78c0ffc546598d0e854dd4f6d543acf7185631")]
 [assembly: System.Reflection.AssemblyProductAttribute("crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
